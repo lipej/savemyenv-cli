3f1bd2f606037034162cbb204a13a2dd1755f38f
Felipe José <lipejose@icloud.com>
2022-01-08 13:47:24 -0300

    feat: types

---
+++ lib/types/index.ts
@@ -0,0 +1,10 @@
+export type PartialDbData = {
+  dbSecret: string;
+  subscription: string;
+  firstName: string;
+  secondName: string;
+};
+
+export type PartialPackageJSON = {
+  name: string;
+};
