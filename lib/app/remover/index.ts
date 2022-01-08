46a00beba09f753f0bbc19cc8213ad0c36b83dd9
Felipe José <lipejose@icloud.com>
2022-01-08 15:40:13 -0300

    feat: save & remove from cloud

---
+++ lib/app/remover/index.ts
@@ -0,0 +1,15 @@
+import fetch from "cross-fetch";
+import { SERVER_URI } from "../../config";
+
+export async function remove(key: string, appName: string) {
+  try {
+    await fetch(SERVER_URI + "/" + appName, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + key,
+      },
+    });
+  } catch (e) {
+    throw e;
+  }
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,11 @@
 import { Command } from "commander";
 import { auth } from "./app";
-import { getAppName, getKey, getEnv } from "./app/getters";
-import { readPackage } from "./app/readers";
+import { decryptEnv } from "./app/decryptors";
+import { encryptEnv } from "./app/encryptors";
+import { getAppName, getKey, getEnv, getSecret } from "./app/getters";
+import { readConfig, readEnv, readPackage } from "./app/readers";
+import { remove } from "./app/remover";
+import { saveEnv } from "./app/savers";
 import { writeConfig, writeEnv, writeKey } from "./app/writters";
 const VERSION = require("../package.json").version;
 
@@ -11,34 +15,82 @@ program.version(VERSION);
 
 program
   .command("auth")
-  .argument("<key>", "provide you auth key")
+  .argument("<key>", "provide you auth key from Save My ENV")
   .action(async (key) => {
     try {
       const configuration = await auth(key);
       writeConfig(configuration);
       writeKey(key);
-      console.info("Auth made sucessfully");
+      console.info("successfully authenticated");
     } catch (err) {
       console.error(err);
     }
   });
 
 program
-  .command("getEnv")
+  .command("getEnv", "get your env from Save My ENV server")
   .argument("<localPass>", "local pass provided when env was uploaded")
   .action(async (localPass) => {
+    try {
+      const key = getKey();
+      const packageFile = readPackage();
+      const secret = getSecret(readConfig());
+
+      if (packageFile) {
+        const appName = getAppName(packageFile);
+        const envEncrypted = await getEnv(key, appName);
+
+        const env = decryptEnv(envEncrypted, secret + localPass);
+
+        writeEnv(env);
+      }
+
+      console.info(".env successfully written");
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+program
+  .command("saveEnv", "save your env to Save My ENV server")
+  .argument("<localPass>", "local pass provided when env was uploaded")
+  .action(async (localPass) => {
+    try {
+      const key = getKey();
+      const secret = getSecret(readConfig());
+      const packageFile = readPackage();
+
+      if (packageFile) {
+        const appName = getAppName(packageFile);
+        const env = readEnv();
+
+        if (env) {
+          const envEncrypted = encryptEnv(env, secret + localPass);
+
+          await saveEnv(key, appName, envEncrypted);
+        }
+      }
+
+      console.info(".env successfully send to the cloud");
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+program
+  .command("remove", "remove your env from Save My ENV cloud server")
+  .action(async () => {
     try {
       const key = getKey();
       const packageFile = readPackage();
 
       if (packageFile) {
         const appName = getAppName(packageFile);
-        const env = await getEnv(key, appName);
 
-        writeEnv(env);
+        await remove(key, appName);
       }
 
-      console.info("Env sucessfully write");
+      console.info(".env successfully deleted from cloud");
     } catch (e) {
       console.log(e);
     }
